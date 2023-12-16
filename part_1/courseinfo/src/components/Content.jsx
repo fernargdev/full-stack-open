c6f21dc3239c29305bce38782b76428a375981cd
fernargdev <115408504+fernargdev@users.noreply.github.com>
2023-12-15 22:42:15 -0500

    part_1-1.2

--- part_1/courseinfo/src/components/Content.jsx
+++ part_1/courseinfo/src/components/Content.jsx
@@ -1,15 +1,11 @@
+import Part from './Part'
+
 const Content = ({ part1, exercise1, part2, exercise2, part3, exercise3 }) => {
   return (
     <main>
-      <p>
-        {part1} {exercise1}
-      </p>
-      <p>
-        {part2} {exercise2}
-      </p>
-      <p>
-        {part3} {exercise3}
-      </p>
+      <Part part={part1} exercise={exercise1} />
+      <Part part={part2} exercise={exercise2} />
+      <Part part={part3} exercise={exercise3} />
     </main>
   )
 }
---
+++ part_1/courseinfo/src/components/Part.jsx
@@ -0,0 +1,11 @@
+const Part = ({ part, exercise }) => {
+  return (
+    <div>
+      <p>
+        {part} {exercise}
+      </p>
+    </div>
+  )
+}
+
+export default Part

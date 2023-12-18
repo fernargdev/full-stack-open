5cd32cb8082d6b176870375bba00c1cb192d2510
fernargdev <115408504+fernargdev@users.noreply.github.com>
2023-12-17 23:31:42 -0500

    part_1-1.14

--- part_1/anecdotes/src/App.jsx
+++ part_1/anecdotes/src/App.jsx
@@ -14,13 +14,25 @@ const App = () => {
     setVotes(newVotes)
   }
 
+  const mostVoted = votes.indexOf(Math.max(...votes))
+
   return (
-    <main>
-      <p>{anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
-      <button onClick={handleVote}>vote</button>
-      <button onClick={handleNext}>next anecdote</button>
-    </main>
+    <>
+      <header>
+        <h2>Anecdote of the day</h2>
+      </header>
+      <main>
+        <p>{anecdotes[selected]}</p>
+        <p>has {votes[selected]} votes</p>
+        <button onClick={handleVote}>vote</button>
+        <button onClick={handleNext}>next anecdote</button>
+      </main>
+      <footer>
+        <h2>Anecdote with most votes</h2>
+        <p>{anecdotes[mostVoted]}</p>
+        <p>has {votes[mostVoted]} votes</p>
+      </footer>
+    </>
   )
 }
 

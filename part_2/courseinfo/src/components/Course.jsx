23f5a90f401f6feab13231a35c7f3a41fb226157
fernargdev <115408504+fernargdev@users.noreply.github.com>
2023-12-19 13:33:52 -0500

    part_2-2.2

--- part_2/courseinfo/src/components/Course.jsx
+++ part_2/courseinfo/src/components/Course.jsx
@@ -1,11 +1,13 @@
 import Header from './Header'
 import Content from './Content'
+import Total from './Total'
 
 const Course = ({ course }) => {
   return (
     <>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   )
 }
--- part_2/courseinfo/src/components/Total.jsx
+++ part_2/courseinfo/src/components/Total.jsx
@@ -1,11 +1,13 @@
-const Total = (props) => {
-  const exercise1 = props.course.parts[0].exercises
-  const exercise2 = props.course.parts[1].exercises
-  const exercise3 = props.course.parts[2].exercises
+const Total = ({ parts }) => {
+  let total = 0
+
+  parts.forEach((part) => {
+    total = total + part.exercises
+  })
 
   return (
     <footer>
-      <p>Number of exercises {exercise1 + exercise2 + exercise3}</p>
+      <strong>total of {total} exercise</strong>
     </footer>
   )
 }

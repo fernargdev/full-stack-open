3c54d7e28d152ff1314ea6939af24308c1b7e418
fernargdev <115408504+fernargdev@users.noreply.github.com>
2023-12-22 18:37:51 -0500

    part_2-2.8

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [persons, setPersons] = useState([
+    // { name: 'Arto Hellas', number: '040-123456' },
+    // { name: 'Ada Lovelace', number: '39-44-5323523' },
+    // { name: 'Dan Abramov', number: '12-43-234345' },
+    // { name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
-
-    const personObject = {
-      name: newName,
-    }
 
     if (persons.find((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
@@ -17,11 +19,19 @@ const App = () => {
       return
     }
 
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    }
+
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => setNewName(event.target.value)
+
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
 
   return (
     <>
@@ -32,7 +42,12 @@ const App = () => {
       <main>
         <form onSubmit={addName}>
           <div>
-            name: <input value={newName} onChange={handleNameChange} />
+            <label>Name: </label>
+            <input type="text" value={newName} onChange={handleNameChange} />
+          </div>
+          <div>
+            <label>Number: </label>
+            <input type="tel" value={newNumber} onChange={handleNumberChange} />
           </div>
           <div>
             <button type="submit">add</button>
@@ -44,12 +59,15 @@ const App = () => {
         <h2>Numbers</h2>
         <ul>
           {persons.map((person) => (
-            <li key={person.name}>{person.name}</li>
+            <li key={person.name}>
+              {person.name}: {person.number}
+            </li>
           ))}
         </ul>
       </footer>
 
       <div>newName: {newName}</div>
+      <div>newNumber: {newNumber}</div>
     </>
   )
 }

8a91f4b0f2d469a5b4216515d81cc6aeeaaf0f30
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-01-03 12:13:21 -0500

    part_3-3.3

--- part_3/phonebook/index.js
+++ part_3/phonebook/index.js
@@ -30,10 +30,6 @@ app.get('/', (request, response) => {
   response.send('<h1> Hello World! </h1>')
 })
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
-
 app.get('/info', (request, response) => {
   response.send(
     `
@@ -46,7 +42,31 @@ app.get('/info', (request, response) => {
   )
 })
 
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find((person) => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port: http://localhost:${PORT}`)
 })
+
+// 3.3: Paso 3 del backend de la agenda telefónica
+// Implemente la funcionalidad para mostrar la información de
+// una única entrada de la agenda telefónica. La URL para
+// obtener los datos de una persona con ID 5 debe ser
+// http://localhost:3001/api/persons/5
+
+// Si no se encuentra una entrada para la identificación dada,
+// el servidor debe responder con el código de estado apropiado.

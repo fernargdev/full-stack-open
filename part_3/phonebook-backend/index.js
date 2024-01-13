739a22fc0504650ccb0a46159af8139e0d5f3c53
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-01-13 11:17:16 -0500

    part_3-3.17

--- part_3/phonebook-backend/index.js
+++ part_3/phonebook-backend/index.js
@@ -117,6 +117,21 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch((error) => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch((error) => next(error))
+})
+
 app.use(uknownEnpoint)
 app.use(errorHandler)
 

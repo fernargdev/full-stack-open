f44af2ea9af6608a450cb91d276c8a9865f1ff6f
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-02-06 19:27:19 -0500

    part_4-4.11

--- part_4/bloglist/controllers/blogs.js
+++ part_4/bloglist/controllers/blogs.js
@@ -1,29 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-// Rutes
-// blogsRouter.get('/', (request, response) => {
-//   Blog.find({}).then((blogs) => {
-//     response.json(blogs)
-//   })
-// })
-
-// New Rutes
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs)
 })
 
-// blogsRouter.post('/', (request, response) => {
-//   const blog = new Blog(request.body)
-
-//   blog.save().then((result) => {
-//     response.status(201).json(result)
-//   })
-// })
-
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
+
+  if (!blog.likes) blog.likes = 0
 
   const result = await blog.save()
   response.status(201).json(result)
--- part_4/bloglist/tests/blog_api.test.js
+++ part_4/bloglist/tests/blog_api.test.js
@@ -75,6 +75,26 @@ describe('tests the endpoint that adds a blog', () => {
     expect(response.body).toHaveLength(initialBlogs.length + 1)
     expect(response.body).toContainEqual(expect.objectContaining(newBlog))
   })
+
+  test('likes default value is 0', async () => {
+    const newBlog = {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    }
+
+    const postResponse = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const addedBlog = response.body.find((r) => r.id === postResponse.body.id)
+
+    expect(addedBlog.likes).toBe(0)
+  })
 })
 
 afterAll(async () => {

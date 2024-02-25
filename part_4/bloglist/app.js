d0d4bf08ccef23d743ffc7332dc1b6a59b4bb330
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-02-25 16:23:36 -0500

    part_4-4.18

--- part_4/bloglist/app.js
+++ part_4/bloglist/app.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -28,6 +29,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)

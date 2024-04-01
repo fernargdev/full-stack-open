2e3bec9e83724bcd3ef2f23cb97bca9014c2aa84
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-04-01 18:56:31 -0400

    part_6-6.2

--- part_6/unicafe-redux/src/main.jsx
+++ part_6/unicafe-redux/src/main.jsx
@@ -17,12 +17,12 @@ const App = () => {
   return (
     <div>
       <div>good {store.getState().good}</div>
-      {/* <div>ok {store.getState().ok}</div> */}
-      {/* <div>bad {store.getState().bad}</div> */}
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
       <button onClick={createDispatch('GOOD')}>good</button>
-      {/* <button onClick={createDispatch('OK')}>ok</button> */}
-      {/* <button onClick={createDispatch('BAD')}>bad</button> */}
-      {/* <button onClick={createDispatch('ZERO')}>reset stats</button> */}
+      <button onClick={createDispatch('OK')}>ok</button>
+      <button onClick={createDispatch('BAD')}>bad</button>
+      <button onClick={createDispatch('ZERO')}>reset stats</button>
     </div>
   )
 }
--- part_6/unicafe-redux/src/reducer.js
+++ part_6/unicafe-redux/src/reducer.js
@@ -8,6 +8,10 @@ const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'GOOD':
       return { ...state, good: state.good + 1 }
+    case 'OK':
+      return { ...state, ok: state.ok + 1 }
+    case 'BAD':
+      return { ...state, bad: state.bad + 1 }
     case 'ZERO':
       return initialState
     default:

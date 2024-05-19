d26d8ba5fef6c74c989a0dfd7d449f7545b14974
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-05-19 11:00:52 -0400

    part_7-7.1

---
+++ part_7/routed-anecdotes/src/main.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <App />
+  </Router>
+)

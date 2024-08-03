aa5d72b4c868d7870bcd7b8d55b365a1e5758956
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-08-02 20:52:54 -0400

    config env

--- part_7/bloglist-redux/bloglist-backend/app.js
+++ part_7/bloglist-redux/bloglist-backend/app.js
@@ -31,14 +31,10 @@ app.use(express.static('dist'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-// TODO: TESTING
-// if (process.env.NODE_ENV === 'test') {
-//   const testingRouter = require('./controllers/testing');
-//   app.use('/api/testing', testingRouter);
-// }
-
-const testingRouter = require('./controllers/testing');
-app.use('/api/testing', testingRouter);
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing', testingRouter);
+}
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/blogs', commentsRouter);
--- part_7/bloglist-redux/bloglist-backend/utils/config.js
+++ part_7/bloglist-redux/bloglist-backend/utils/config.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT;
-// const MONGODB_URI = process.env.MONGODB_URI;
 const MONGODB_URI =
   process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI

d5bdfea7bc92ff20f80987681299beece0b7ba33
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-08-05 17:39:42 -0400

    testing

--- part_7/bloglist-query/bloglist-backend/app.js
+++ part_7/bloglist-query/bloglist-backend/app.js
@@ -28,10 +28,10 @@ app.use(express.static('dist'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-// if (process.env.NODE_ENV === 'test') {
-const testingRouter = require('./controllers/testing');
-app.use('/api/testing', testingRouter);
-// }
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing', testingRouter);
+}
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);

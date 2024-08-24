defdafaf4fcd1d2ffe6808c0eae538e5b18ccd29
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-08-24 19:23:14 -0400

    part_9-9.15

--- part_9/react-types/src/App.tsx
+++ part_9/react-types/src/App.tsx
@@ -1,3 +1,189 @@
+interface CoursePartBase {
+  name: string
+  exerciseCount: number
+}
+
+interface CoursePartDescription extends CoursePartBase {
+  description: string
+}
+
+interface CoursePartBasic extends CoursePartDescription {
+  // description: string
+  kind: 'basic'
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number
+  kind: 'group'
+}
+
+interface CoursePartBackground extends CoursePartDescription {
+  // description: string
+  backgroundMaterial: string
+  kind: 'background'
+}
+
+interface CousePartRequirement extends CoursePartDescription {
+  requirements: string[]
+  kind: 'special'
+}
+
+type CoursePart =
+  | CoursePartBasic
+  | CoursePartGroup
+  | CoursePartBackground
+  | CousePartRequirement
+
+interface PartProps {
+  part: CoursePart
+}
+
+const Part = (props: PartProps) => {
+  const part = props.part
+
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    )
+  }
+
+  switch (part.kind) {
+    case 'basic':
+      return (
+        // <p>
+        //   {part.name} {part.exerciseCount} {part.description}
+        // </p>
+
+        <>
+          <b>
+            {part.name} {part.exerciseCount}
+          </b>
+
+          <p
+            style={{
+              fontStyle: 'italic',
+              margin: 0,
+            }}
+          >
+            {part.description}
+          </p>
+        </>
+      )
+
+    case 'group':
+      return (
+        // <p>
+        //   {part.name} {part.exerciseCount} {part.groupProjectCount}
+        // </p>
+
+        <div>
+          <b>
+            {part.name} {part.exerciseCount}
+          </b>
+
+          <p
+            style={{
+              margin: 0,
+            }}
+          >
+            project exercises {part.groupProjectCount}
+          </p>
+        </div>
+      )
+
+    case 'background':
+      return (
+        // <p>
+        //   {part.name} {part.exerciseCount} {part.description}{' '}
+        //   {part.backgroundMaterial}
+        // </p>
+
+        <>
+          <b>
+            {part.name} {part.exerciseCount}
+          </b>
+
+          <p
+            style={{
+              fontStyle: 'italic',
+              margin: 0,
+            }}
+          >
+            {part.description}
+          </p>
+
+          <p
+            style={{
+              margin: 0,
+            }}
+          >
+            {`submit to ${part.backgroundMaterial}`}
+          </p>
+        </>
+      )
+
+    case 'special':
+      return (
+        // <p>
+        //   {part.name} {part.exerciseCount} {part.description}{' '}
+        //   {part.requirements.join(', ')}
+        // </p>
+
+        <>
+          <b>
+            {part.name} {part.exerciseCount}
+          </b>
+
+          <p
+            style={{
+              fontStyle: 'italic',
+              margin: 0,
+            }}
+          >
+            {part.description}
+          </p>
+          <p
+            style={{
+              margin: 0,
+            }}
+          >
+            {`required skills: ${part.requirements.join(', ')}`}
+          </p>
+        </>
+      )
+
+    default:
+      return assertNever(part)
+  }
+}
+
+interface ContentProps {
+  parts: CoursePart[]
+}
+
+const Content = (props: ContentProps) => {
+  return (
+    <main
+      style={{
+        margin: '2.5rem 0',
+      }}
+    >
+      {props.parts.map((part) => {
+        return (
+          <div
+            key={part.name}
+            style={{
+              margin: '1.5rem 0',
+            }}
+          >
+            <Part part={part} />
+          </div>
+        )
+      })}
+    </main>
+  )
+}
+
 interface HeaderProps {
   name: string
 }
@@ -10,52 +196,67 @@ const Header = (props: HeaderProps) => {
   )
 }
 
-interface ContentProps {
-  parts: {
-    name: string
-    exerciseCount: number
-  }[]
-}
-
-const Content = (props: ContentProps) => {
-  return (
-    <main>
-      {props.parts.map((part) => (
-        <p key={part.name}>
-          {part.name} {part.exerciseCount}
-        </p>
-      ))}
-    </main>
-  )
-}
-
 interface TotalProps {
   total: number
 }
 
 const Total = (props: TotalProps) => {
   return (
-    <section>
-      <p>Number of exercises {props.total}</p>
-    </section>
+    <footer
+      style={{
+        fontSize: '1.5rem',
+      }}
+    >
+      <p>
+        Number of exercises:
+        <span>{` ${props.total}`}</span>
+      </p>
+    </footer>
   )
 }
 
 const App = () => {
   const courseName = 'Half Stack application development'
 
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
       exerciseCount: 10,
+      description: 'This is an awesome course part',
+      kind: 'basic',
     },
     {
       name: 'Using props to pass data',
       exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: 'group',
+    },
+    {
+      name: 'Basics of type Narrowing',
+      exerciseCount: 7,
+      description: 'How to go from unknown to string',
+      kind: 'basic',
     },
     {
       name: 'Deeper type usage',
       exerciseCount: 14,
+      description: 'Confusing description',
+      backgroundMaterial:
+        'https://type-level-typescript.com/template-literal-types',
+      kind: 'background',
+    },
+    {
+      name: 'TypeScript in frontend',
+      exerciseCount: 10,
+      description: 'a hard part',
+      kind: 'basic',
+    },
+    {
+      name: 'Backend development',
+      exerciseCount: 21,
+      description: 'Typing the backend',
+      requirements: ['nodejs', 'jest'],
+      kind: 'special',
     },
   ]
 
@@ -64,39 +265,13 @@ const App = () => {
     0
   )
 
-  // const-declarations
-
-  // return (
-  //   <div>
-  //     <Header name={courseName} />
-  //     <Content ... />
-  //     <Total ... />
-  //   </div>
-  // )
-
   return (
-    <div>
+    <>
       <Header name={courseName} />
       <Content parts={courseParts} />
       <Total total={totalExercises} />
-    </div>
+    </>
   )
-
-  // return (
-  //   <div>
-  //     <h1>{courseName}</h1>
-  //     <p>
-  //       {courseParts[0].name} {courseParts[0].exerciseCount}
-  //     </p>
-  //     <p>
-  //       {courseParts[1].name} {courseParts[1].exerciseCount}
-  //     </p>
-  //     <p>
-  //       {courseParts[2].name} {courseParts[2].exerciseCount}
-  //     </p>
-  //     <p>Number of exercises {totalExercises}</p>
-  //   </div>
-  // )
 }
 
 export default App

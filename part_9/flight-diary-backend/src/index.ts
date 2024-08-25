8e8222cfe111cdd3424effbb945c64a9bf6e2d19
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-08-25 09:12:22 -0400

    create flight-diary-backend app

---
+++ part_9/flight-diary-backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+const app = express();
+import diaryRouter from './routes/diaries';
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get('/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.use('/api/diaries', diaryRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

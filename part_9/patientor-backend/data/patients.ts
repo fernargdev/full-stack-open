50846decfa04bdfd11dccda265b680aebfafd259
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-09-02 12:20:36 -0400

    part_9-9.23

--- part_9/patientor-backend/data/patients.ts
+++ part_9/patientor-backend/data/patients.ts
@@ -47,6 +47,75 @@ const data = [
     occupation: 'Digital evangelist',
     entries: [],
   },
+  {
+    id: 'TEST35f8-01f3-4b3a-92b2-731bbc3c15e2',
+    name: 'John McClane1',
+    ssn: '090786-122X',
+    occupation: 'New york city cop',
+    dateOfBirth: '1986-07-09',
+    gender: 'male',
+    entries: [
+      {
+        id: 'd8e235f8-01f3-4b3a-92b2-731bbc3c15e2',
+        date: '2019-08-05',
+        type: 'Hospital',
+        specialist: 'MD House',
+        diagnosisCodes: ['S62.5'],
+        description: 'Padiatria Padiatria Padiatria Padiatria',
+        discharge: {
+          date: '2019-08-16',
+          criteria: 'Padiatria',
+        },
+      },
+    ],
+  },
+  {
+    id: 'TESTe46d-70b3-4d90-b090-4535c7cf8fb1',
+    name: 'John McClane2',
+    ssn: '090786-122X',
+    occupation: 'New york city cop',
+    dateOfBirth: '1986-07-09',
+    gender: 'male',
+    entries: [
+      {
+        id: 'd811e46d-70b3-4d90-b090-4535c7cf8fb1',
+        date: '2015-01-02',
+        type: 'Hospital',
+        specialist: 'MD House',
+        diagnosisCodes: ['S62.5'],
+        description:
+          "Healing time appr. 2 weeks. patient doesn't remember how he got the injury.",
+        discharge: {
+          date: '2015-01-16',
+          criteria: 'Thumb has healed.',
+        },
+      },
+    ],
+  },
+  {
+    id: 'TEST9fa6-c4b4-4fec-ac4d-df4fe1f85f62',
+    name: 'John McClane3',
+    ssn: '090786-122X',
+    occupation: 'New york city cop',
+    dateOfBirth: '1986-07-09',
+    gender: 'male',
+    entries: [
+      {
+        id: 'fcd59fa6-c4b4-4fec-ac4d-df4fe1f85f62',
+        date: '2019-08-05',
+        type: 'OccupationalHealthcare',
+        specialist: 'MD House',
+        employerName: 'HyPD',
+        diagnosisCodes: ['Z57.1', 'Z74.3', 'M51.2'],
+        description:
+          'Patient mistakenly found himself in a nuclear plant waste site without protection gear. Very minor radiation poisoning.',
+        sickLeave: {
+          startDate: '2019-08-05',
+          endDate: '2019-08-28',
+        },
+      },
+    ],
+  },
 ];
 
 const patientEntries: Patient[] = data.map((obj) => {
--- part_9/patientor-backend/src/types.ts
+++ part_9/patientor-backend/src/types.ts
@@ -13,9 +13,6 @@ export enum Gender {
   Other = 'other',
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {}
-
 export interface Patient {
   id: string;
   name: string;
@@ -31,3 +28,49 @@ export type NoSsnPatient = Omit<Patient, 'ssn'>;
 export type NewPatientEntry = z.infer<typeof NewPatientSchema>;
 
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave: SickLeave;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
